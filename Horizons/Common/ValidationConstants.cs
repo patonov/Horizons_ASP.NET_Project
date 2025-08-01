a2463c289f5aa4a80e935b344b8c835fa53a2efd
Nikolay Patonov <patonec@mail.bg>
2025-08-01 20:11:34 +0300

    Add project files.

---
+++ Horizons/Common/ValidationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Horizons.Common
+{
+    public class ValidationConstants
+    {
+        public class DestinationValidationConstants
+        {
+            public const int DestinationNameMaxLength = 80;
+        }
+    }
+}
---
+++ Horizons/Data/Models/Destination.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Horizons.Common.ValidationConstants.DestinationValidationConstants;
+
+namespace Horizons.Data.Models
+{
+    public class Destination
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DestinationNameMaxLength)]
+        public string Name { get; set; } = null!;
+    }
+}
