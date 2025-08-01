f6337183a189f102f4210dd04b79afb45e5b5473
Nikolay Patonov <patonec@mail.bg>
2025-08-01 20:11:34 +0300

    Add project files.

---
+++ Horizons/Data/Models/Terrain.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Horizons.Data.Models
+{
+    public class Terrain
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public ICollection<Destination> Destinations { get; set; } = new List<Destination>();
+    }
+}
